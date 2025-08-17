b94246262393d551d7e634b44aec898d445220ae
Teh Xin Hui <158593330+GREYHAT262@users.noreply.github.com>
2025-08-17 16:36:21 +0800

    Add respond function
    Creates fixed template for printing output and reduces clutter

--- src/main/java/Gray.java
+++ src/main/java/Gray.java
@@ -1,11 +1,12 @@
 public class Gray {
-    public static void main(String[] args) {
+    public static void respond(String input) {
         String horizontalLine = "----------------------------------------------------------";
-        System.out.println(horizontalLine + "\n" + """
+        System.out.println(horizontalLine + "\n" + input + "\n" + horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        Gray.respond("""
                 Hello! I'm Gray.
-                What can I do for you?
-                """ + horizontalLine + "\n" + """
-                Bye. Hope to see you again soon!
-                """ + horizontalLine);
+                What can I do for you?""");
     }
 }
