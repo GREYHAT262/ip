0a92ad53c3dadebb580b6cdc78e71c19b8754d76
Teh Xin Hui <158593330+GREYHAT262@users.noreply.github.com>
2025-08-17 13:31:55 +0800

    Create skeletal version that greets and exits

--- src/main/java/Gray.java
+++ src/main/java/Gray.java
@@ -1,10 +1,11 @@
-public class Duke {
+public class Gray {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "----------------------------------------------------------";
+        System.out.println(horizontalLine + "\n" + """
+                Hello! I'm Gray.
+                What can I do for you?
+                """ + horizontalLine + "\n" + """
+                Bye. Hope to see you again soon!
+                """ + horizontalLine);
     }
 }
