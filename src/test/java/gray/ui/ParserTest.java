25a088a4498602245d913b33923fd6a20f9c3552
Teh Xin Hui <158593330+GREYHAT262@users.noreply.github.com>
2025-09-19 12:18:23 +0800

    Add JUnit tests for getResponse method in Gray.java

--- src/test/java/gray/ui/ParserTest.java
+++ src/test/java/gray/ui/ParserTest.java
@@ -33,6 +33,10 @@ public class ParserTest {
     @Test
     public void parse_stringWithMark_returnMarkCommand() {
         assertInstanceOf(MarkCommand.class, Parser.parse("mark 2"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidMark_returnNoIndexCommand() {
         assertInstanceOf(NoIndexCommand.class, Parser.parse("mark"));
         assertInstanceOf(NoIndexCommand.class, Parser.parse("mark two"));
     }
@@ -40,6 +44,10 @@ public class ParserTest {
     @Test
     public void parse_stringWithUnmark_returnUnmarkCommand() {
         assertInstanceOf(UnmarkCommand.class, Parser.parse("unmark 2"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidUnmark_returnNoIndexCommand() {
         assertInstanceOf(NoIndexCommand.class, Parser.parse("unmark"));
         assertInstanceOf(NoIndexCommand.class, Parser.parse("unmark three"));
     }
@@ -47,16 +55,28 @@ public class ParserTest {
     @Test
     public void parse_stringWithTodo_returnAddCommand() {
         assertInstanceOf(AddCommand.class, Parser.parse("todo borrow book"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidTodo_returnInvalidTaskExceptionCommand() {
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("todo"));
     }
 
     @Test
     public void parse_stringWithDeadline_returnAddCommand() {
         assertInstanceOf(AddCommand.class, Parser.parse("deadline return book /by 2025-08-26 2359"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidDeadline_returnInvalidTaskExceptionCommand() {
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("deadline"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("deadline return book"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("deadline /by 2025-08-26 2359"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("deadline /by"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidDeadlineDateTime_returnInvalidDateTimeCommand() {
         assertInstanceOf(InvalidDateTimeCommand.class, Parser.parse("deadline return book /by Sunday"));
     }
 
@@ -64,12 +84,20 @@ public class ParserTest {
     public void parse_stringWithEvent_returnAddCommand() {
         assertInstanceOf(AddCommand.class, Parser.parse(
                 "event marathon /from 2025-08-26 0800 /to 2025-08-26 1200"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidEvent_returnInvalidTaskExceptionCommand() {
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("event"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("event marathon"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("event marathon /from 2025-08-26 0800"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse("event marathon /to 2025-08-26 1200"));
         assertInstanceOf(InvalidTaskExceptionCommand.class, Parser.parse(
                 "event /from 2025-08-26 0800 /to 2025-08-26 1200"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidEventDateTime_returnInvalidDateTimeCommand() {
         assertInstanceOf(InvalidDateTimeCommand.class, Parser.parse(
                 "event marathon /from Sunday 8am /to Sunday 12pm"));
     }
@@ -77,6 +105,10 @@ public class ParserTest {
     @Test
     public void parse_stringWithDelete_returnDeleteCommand() {
         assertInstanceOf(DeleteCommand.class, Parser.parse("delete 2"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidDelete_returnNoIndexCommand() {
         assertInstanceOf(NoIndexCommand.class, Parser.parse("delete"));
         assertInstanceOf(NoIndexCommand.class, Parser.parse("delete three"));
     }
@@ -84,7 +116,15 @@ public class ParserTest {
     @Test
     public void parse_stringWithDate_returnDateCommand() {
         assertInstanceOf(DateCommand.class, Parser.parse("date 2025-08-26"));
+    }
+
+    @Test
+    public void parse_stringWithNoDate_returnNoDateCommand() {
         assertInstanceOf(NoDateCommand.class, Parser.parse("date"));
+    }
+
+    @Test
+    public void parse_stringWithInvalidDate_returnInvalidDateCommand() {
         assertInstanceOf(InvalidDateCommand.class, Parser.parse("date Sunday"));
     }
 
